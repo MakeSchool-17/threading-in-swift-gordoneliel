0b032819ff4f7488db402bdd2cea96248bb05302
gordoneliel <gordoneliel@gmail.com>
2015-11-18 00:30:07 -0800

    Added Group threads

--- MultiThreadingSwift/ViewController.swift
+++ MultiThreadingSwift/ViewController.swift
@@ -9,36 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  @IBOutlet var downloadStatusLabel: UILabel!
-  @IBOutlet var stringDisplayLabel: UILabel!
-  
-  let strings = ["Test1", "Test2", "Test3"]
-  var currentStringIndex = 0
-  var operationCount = 20
-  
-  override func viewDidAppear(animated: Bool) {
-    super.viewDidAppear(animated)
     
-    downloadData()
-  }
-  
-  @IBAction func nextStringButtonTapped(sender: AnyObject) {
-    currentStringIndex++
-    currentStringIndex = currentStringIndex < strings.count ? currentStringIndex : 0
+    @IBOutlet var downloadStatusLabel: UILabel!
+    @IBOutlet var stringDisplayLabel: UILabel!
     
-    stringDisplayLabel.text = strings[currentStringIndex]
-  }
-  
-  func downloadData() {
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH,0)) {
-        for i in 1...self.operationCount {
-            self.downloadStatusLabel.text = "\(i)/\(self.operationCount)"
-            sleep(1)
+    let strings = ["Test1", "Test2", "Test3"]
+    var currentStringIndex = 0
+    var operationCount = 1000
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        downloadData()
+    }
+    
+    @IBAction func nextStringButtonTapped(sender: AnyObject) {
+        currentStringIndex++
+        currentStringIndex = currentStringIndex < strings.count ? currentStringIndex : 0
+        
+        stringDisplayLabel.text = strings[currentStringIndex]
+    }
+    
+    func downloadData() {
+        let dispatch_groups = dispatch_group_create()
+        
+        for _ in 1...self.operationCount {
+            dispatch_group_async(dispatch_groups, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0)) { i in
+                self.downloadStatusLabel.text = "\(i)/\(self.operationCount)"
+                sleep(1)
+            }
         }
         
+        dispatch_group_wait(dispatch_groups, DISPATCH_TIME_FOREVER)
         self.downloadStatusLabel.text = "Completed!"
+        
+        
     }
-  }
 }
 
